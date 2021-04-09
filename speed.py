e228e4d77e4058e35b90d0474553edd995a2c102
AJ <70701724+s11341757@users.noreply.github.com>
2021-04-09 14:10:05 -0400

    Do Math
    first commit

---
+++ speed.py
@@ -0,0 +1,16 @@
+# Speed Calculato, AJ Dues, April 09, 2021, 1:44PM v0.0
+
+keep_going = "Yes"
+
+while keep_going == "Yes" or keep_going == "yes":
+
+    distance = float(input("Please enter the distance and press enter.\n"))
+    time = float(input("Please enter the time and press enter.\n"))
+
+    speed = distance / time
+
+    print(f"Using the distance of {distance} and time of {time} seconds, the speed is {speed} m/s.\n" )
+
+    keep_going = input("Would you like to do another calculation? Type yes or no and push enter.\n")
+
+print("Thank you for using this calculator.\n")+
