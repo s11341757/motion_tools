93b19a85b14547f1e088284e2f00e65617bbb864
AJ <70701724+s11341757@users.noreply.github.com>
2021-04-09 14:20:51 -0400

    [8B] v0.1
    Speed calculator

--- speed.py
+++ speed.py
@@ -1,4 +1,4 @@
-# Speed Calculato, AJ Dues, April 09, 2021, 1:44PM v0.0
+# Speed Calculator, AJ Dues, April 09, 2021, 2:19PM v0.1
 
 keep_going = "Yes"
 
@@ -6,10 +6,13 @@ while keep_going == "Yes" or keep_going == "yes":
 
     distance = float(input("Please enter the distance and press enter.\n"))
     time = float(input("Please enter the time and press enter.\n"))
-fbhnujrigjeogbherigh
+
+    speed = distance / time
 
     print(f"Using the distance of {distance} and time of {time} seconds, the speed is {speed} m/s.\n" )
 
     keep_going = input("Would you like to do another calculation? Type yes or no and push enter.\n")
 
-print("Thank you for using this calculator.\n")+
+print("Thank you for using this calculator.\n")
+
+
